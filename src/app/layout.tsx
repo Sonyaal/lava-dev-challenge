cf66801b1ba88f95c1843e4355825f477ca13f8d
Sonya Alexis <salexis@usc.edu>
2025-02-07 03:22:51 -0800

    updating description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import localFont from 'next/font/local'
- 
-// Font files can be colocated inside of `app`
+
 const myFont = localFont({
   src: "../../public/fonts/UncutSans-Variable.ttf",
   display: 'swap',
 })
- 
 
 export const metadata = {
   title: 'Tally',
-  description: 'Tally is a inventory management system.',
+  description: 'Tally is an inventory management system.',
 };
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
